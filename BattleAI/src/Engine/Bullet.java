e2fa731370587a0c7aa3eab6509ad13eaf384098
Tanase Mihai Andrei <sulzandreig@gmail.com>
2016-03-17 15:11:16 +0200

    Moved files for game engine in game Engine folder

---
+++ BattleAI/src/Engine/Bullet.java
@@ -0,0 +1,6 @@
+
+package GameObjects;
+
+public class Bullet extends GameEntity{
+    
+}
---
+++ BattleAI/src/Engine/GameEntity.java
@@ -0,0 +1,8 @@
+
+package GameObjects;
+import java.awt.geom.*;
+public class GameEntity {
+    Rectangle2D.Double entity = new Rectangle2D.Double(10,10,10,10);
+    double x,y;
+    int speed;
+}
