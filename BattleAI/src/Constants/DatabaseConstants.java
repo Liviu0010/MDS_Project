dd598bc873f8fbb823e9458706832f80bdaaef56
Dragos-Alexandru Gabor <gabordragos@gmail.com>
2016-03-17 02:09:48 +0200

    Merged the two projects into one, and separated them in main by the given arguments upon opening the application (ApplicationState) Added new packages with dummy classes for orientation (shall be deleted when a must)

---
+++ BattleAI/src/Constants/DatabaseConstants.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Constants;
+
+/**
+ *
+ * @author Dragos-Alexandru
+ */
+public interface DatabaseConstants {
+    String username = "";
+    String password = "";
+}
