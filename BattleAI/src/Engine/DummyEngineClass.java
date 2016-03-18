e7ee97cefc61e7eb0f28738f462f18d5d76c4961
Tanase Mihai Andrei <sulzandreig@gmail.com>
2016-03-18 16:09:12 +0200

    Modified all classes and interfaces with some added methods

--- BattleAI/src/Engine/DummyEngineClass.java
+++ BattleAI/src/Engine/DummyEngineClass.java
@@ -4,6 +4,7 @@ package Engine;
 public class DummyEngineClass {
     public static  void main(String arg[]){
         Bullet b;
+        Tank t = new Tank();
         System.out.print("Engine Main");
     }
 }
