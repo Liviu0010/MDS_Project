12f487ef8dcfaa8d9c4e25ea304c3bf494797ad2
Alexandru Ghergut <alexandru.ghergut94@gmail.com>
2016-04-13 11:46:19 +0300

    Remove unused imports

--- BattleAIMaven/src/main/java/Networking/Server/PlayerConnection.java
+++ BattleAIMaven/src/main/java/Networking/Server/PlayerConnection.java
@@ -6,9 +6,7 @@
 package Networking.Server;
 
 import Constants.MasterServerConstants;
-import Networking.Requests.HostMatch;
 import Networking.Requests.Request;
-import Networking.Requests.RequestType;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.Timer;
