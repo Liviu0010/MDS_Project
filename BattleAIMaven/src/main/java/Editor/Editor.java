f248d069678fd6629c89919f0e42029a073daa48
Gabor132 <gabordragos@gmail.com>
2016-04-24 23:04:01 +0300

    Added full functionality to Editor (#63)
    * Added LoginWorker and Guard in Security package
    
    * Added forgoten Guard class
    
    * Added user class
    Added more list interactions in MultiplayerMatchPanel and SinglePlayerPanel
    Changed remove to ready in MultiplayerMatchPanel
    Changed add to select in MutliplayerMatchPanel
    Added HelloMessage in MultiplayerServerPanel
    Added login and logout in MainFrame
    Added new toListMatch function in matches
    Added new toListSource function in sources
    Added getSourceList in SourceManager and it writes some test sources
    Added MAX_TANK_NUMBER in singlePlayer (VisualConstants) at 8
    Added some error messaging in DatabaseHandler
    
    * Added ListenerWorker
    
    * Rewrote swing workers
    
    * Added ConsoleFrame commands (to see them, type /help in ConsoleFrame)
    Added lineWrap to outputArea's
    Added enter feature to inputField's
    Added autoscroll when content is bigger than outputArea's
    
    * Added the compile class which instantiates a given source.
    
    * Added Compile functionality to Editor and CompileArea where errors are displayed.
    
    * Commit from rapsberry, set default username
    
    * Removede server_console_db from git
    
    * Removede server_console_db from git
    
    * MASSIVE COMMIT:
    
    Added functionality to ALL buttons in the Editor.java
    Refactored SourceManager and SourceCompiler and added javadoc to them.

--- BattleAIMaven/src/main/java/Editor/Editor.java
+++ BattleAIMaven/src/main/java/Editor/Editor.java
@@ -5,12 +5,23 @@
  */
 package Editor;
 
+import Compiler.SourceCompiler;
+import Console.ConsoleFrame;
+import Constants.PathConstants;
 import Interface.MainFrame;
+import Networking.Server.Player;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Calendar;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
@@ -18,8 +29,8 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
-import javax.swing.JTextPane;
 import javax.swing.ScrollPaneConstants;
+import javax.swing.filechooser.FileFilter;
 import javax.swing.plaf.BorderUIResource;
 import org.fife.ui.rsyntaxtextarea.RSyntaxDocument;
 import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
@@ -45,19 +56,17 @@ public final class Editor extends JFrame {
     private final JScrollPane scrollCompileResult;
     private final JPanel sourcePanel;
     
-    
-    private final String sourceName;
+    private static String sourceName;
     
     /**
      * 
      * @param mainFrame
      */
     public Editor(MainFrame mainFrame){
-        super("editor");
+        super("Editor - (unsaved)");
         
         this.frame = mainFrame;
         inteligenceTemplate = SourceManager.getInstance().getIntelligenceTemplate();
-        
         sourceName = JOptionPane.showInputDialog("Source name: ", "LocalSource");
         
         inteligenceTemplate = inteligenceTemplate.replaceFirst("<name>", sourceName);
@@ -73,8 +82,10 @@ public final class Editor extends JFrame {
         sourceArea.setActiveLineRange(3, 10);
         sourceArea.setBorder(new BorderUIResource.LineBorderUIResource(Color.white, 4));
         
+        
         scrollSourceArea = new JScrollPane();
         scrollSourceArea.setViewportView(sourceArea);
+        scrollSourceArea.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
         
         compileResult = new JTextArea();
         compileResult.setEditable(false);
@@ -87,29 +98,29 @@ public final class Editor extends JFrame {
         scrollCompileResult.setViewportView(compileResult);
         scrollCompileResult.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
         
-        sourcePanel = new JPanel(new GridLayout(2, 1));
+        sourcePanel = new JPanel(new GridLayout(2,1));
         
         EditorMenuListener editorMenuListener = new EditorMenuListener();
         
         newButton = new JMenuItem("New");
         newButton.setBackground(Color.BLACK);
         newButton.setForeground(Color.WHITE);
-        newButton.addMouseListener(editorMenuListener);
+        newButton.addMouseListener(new EditorNewListener());
         
         openButton = new JMenuItem("Open");
         openButton.setBackground(Color.BLACK);
         openButton.setForeground(Color.WHITE);
-        openButton.addMouseListener(editorMenuListener);
+        openButton.addMouseListener(new EditorOpenListener());
         
         saveButton = new JMenuItem("Save");
         saveButton.setBackground(Color.BLACK);
         saveButton.setForeground(Color.WHITE);
-        saveButton.addMouseListener(editorMenuListener);
+        saveButton.addMouseListener(new EditorSaveListener());
         
         compileButton = new JMenuItem("Compile");
         compileButton.setBackground(Color.BLACK);
         compileButton.setForeground(Color.WHITE);
-        compileButton.addMouseListener(new EditorCompileListener(sourceName, sourceArea, compileResult));
+        compileButton.addMouseListener(new EditorCompileListener());
         
         menuBar = new JMenuBar();
         menuBar.setLayout(new GridLayout(1,4));
@@ -130,7 +141,7 @@ public final class Editor extends JFrame {
      */
     public final void start(){
         
-        this.setSize(800, 800);
+        this.setSize(800, 600);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         this.setLayout(new BorderLayout());
@@ -143,10 +154,120 @@ public final class Editor extends JFrame {
         }
         });
         
-        this.setVisible(true);
         this.add(menuBar, BorderLayout.NORTH);
-        sourcePanel.add(scrollSourceArea);
-        sourcePanel.add(scrollCompileResult);
+        sourcePanel.add(scrollSourceArea,BorderLayout.CENTER);
+        sourcePanel.add(scrollCompileResult,BorderLayout.SOUTH);
         this.add(sourcePanel, BorderLayout.CENTER);
+        
+        this.setVisible(true);
     }
+    
+    private final class EditorNewListener extends EditorMenuListener{
+        
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            String inteligenceTemplate = SourceManager.getInstance().getIntelligenceTemplate();
+            sourceName = JOptionPane.showInputDialog("Source name: ", "LocalSource");
+
+            inteligenceTemplate = inteligenceTemplate.replaceFirst("<name>", sourceName);
+
+            sourceArea.setText("");
+
+            sourceArea.append(inteligenceTemplate);
+        }
+    }
+    private final class EditorOpenListener extends EditorMenuListener{
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            
+            int external;
+            external = JOptionPane.showConfirmDialog(null, "Is this file outside of our application?", "File chooser", JOptionPane.YES_NO_OPTION);
+            JFileChooser fileChooser;
+            if(external == JOptionPane.NO_OPTION){
+                fileChooser = new JFileChooser(new File(PathConstants.USER_SOURCES_INDEX_PATH));
+            }else{
+                fileChooser = new JFileChooser(new File(System.getProperty("user.home"), "Desktop"));
+            }
+            fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+            fileChooser.setFileFilter(new FileFilter() {
+                @Override
+                public boolean accept(File f) {
+                    return f.getAbsolutePath().endsWith(".java") ||
+                            f.getAbsolutePath().endsWith(".txt") ||
+                            !f.getAbsolutePath().contains(".");
+                }
+
+                @Override
+                public String getDescription() {
+                    return "Java source,text file (*.java;*.txt)";
+                }
+            });
+
+            int resultVal = fileChooser.showOpenDialog(null);
+            if(resultVal == JFileChooser.APPROVE_OPTION){
+                File openedFile = fileChooser.getSelectedFile();
+                String sourceNameAux = openedFile.getName();
+
+                if(sourceNameAux.endsWith(".java")){
+                    sourceName = sourceNameAux.replaceFirst(".java", "");
+                }else{
+                    sourceName = sourceNameAux.replaceFirst(".txt", "");
+                }
+
+                String fileContent = "";
+                try(FileReader fileReader = new FileReader(openedFile);
+                        BufferedReader reader = new BufferedReader(fileReader)){
+                    while(reader.ready()){
+                        fileContent += "\n"+reader.readLine();
+                    }
+                }catch(IOException ex) {
+                    ConsoleFrame.sendMessage(this.getClass().getSimpleName(), "Failed to read from opened source");
+                }
+                sourceArea.setText(fileContent);
+            }
+            saveButton.setForeground(Color.red);
+        }
+    }
+    private final class EditorSaveListener extends EditorMenuListener{
+        
+        @Override
+        public void mouseClicked(MouseEvent e) {
+
+            Source source = new Source(sourceArea.getText(), sourceName, Player.getInstance().getUsername());
+
+            boolean success = SourceManager.getInstance().saveFileToSourceFolder(source);
+
+            if(success){
+                JOptionPane.showMessageDialog(null, "Save succesfull");
+            }else{
+                JOptionPane.showMessageDialog(null, "Save failed");
+            }
+            saveButton.setForeground(Color.WHITE);
+        }
+    }
+    private final class EditorCompileListener extends EditorMenuListener{
+        
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            Calendar timePressed = Calendar.getInstance();
+
+            Source source = new Source(sourceArea.getText(),sourceName, Player.getInstance().getUsername());
+            Object sourceObject = SourceCompiler.getInstanceOfSource(source);
+
+            boolean success = false;
+            if(sourceObject != null){
+                success = true;
+            }
+
+            compileResult.setText(" Compile result for " +
+                    sourceName + ".java at <" + timePressed.getTime().toString() + "> : \n");
+            if(success){
+                compileResult.append("\nCompilation was successfull");
+            }else{
+                compileResult.append("\nCompilation failed: \n"+SourceCompiler.getLastError());
+            }
+        }
+    }
+    
 }
