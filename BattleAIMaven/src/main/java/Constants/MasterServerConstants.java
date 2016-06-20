d415aab08e7d02aa87c8386441f6c1f57782d336
Dragos <gabordragos@gmail.com>
2016-06-20 11:17:01 +0300

    Made it work with travis.

--- BattleAIMaven/src/main/java/Constants/MasterServerConstants.java
+++ BattleAIMaven/src/main/java/Constants/MasterServerConstants.java
@@ -8,7 +8,17 @@ import javax.swing.JOptionPane;
  */
 public interface MasterServerConstants {
     String MASTER_USERNAME = "BattleAIMS";
-    String IP = JOptionPane.showInputDialog(null, "Master Server IP: ", "MS - IP", JOptionPane.INFORMATION_MESSAGE);
+    String IP = getIP();
     int PORT = 60010;
     long PACKET_DELAY = 3000; // expressed in milliseconds
+    
+    public static String getIP(){
+        if(System.getenv().get("TRAVIS") != null){
+            return "localhost";
+        }else{
+            return JOptionPane.showInputDialog(null, "Master Server IP: ",
+                    "MS - IP", JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+    
 }
